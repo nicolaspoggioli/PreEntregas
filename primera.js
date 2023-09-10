c502ebba3530b66a63d1a4a5c4e1744ec0349503
Nicolas Poggioli <nicolas.poggioli.funk@gmail.com>
2023-09-10 14:14:53 -0300

    primera vez

---
+++ primera.js
@@ -0,0 +1,28 @@
+
+const zonas = ["caba", "oeste","norte", "sur"]
+
+console.log (zonas)
+
+
+function calcularEnvio () {
+let remitente = prompt ("Indique su zona de residencia (caba, oeste, norte o sur").toLowerCase();
+
+if (remitente == zonas[0]) {
+    alert ("el costo del envio es de 1000")
+} else if (remitente == zonas[1]) {
+    alert ("el costo del envio es de 1500")
+}
+else if (remitente == zonas[1]) {
+    alert ("el costo del envio es de 1800")
+}
+else if (remitente == zonas[2]) {
+    alert ("el costo del envio es de 1300")
+}
+else if (remitente == zonas[3]) {
+    alert ("el costo del envio es de 2500")
+}
+else 
+{alert ("ingrese un dato valido, escoga entre caba, norte, oeste y sur"); calcularEnvio()}
+}
+
+calcularEnvio ()
